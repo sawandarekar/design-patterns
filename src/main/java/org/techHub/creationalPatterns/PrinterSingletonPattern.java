9d0c074fa9c2c987aa5f5f9e0cbfba8c511d763f
TTG\darekar <sawandarekar@gmail.com>
2017-12-03 15:35:44 +0530

    singleton pattern updated

--- src/main/java/org/techHub/creationalPatterns/PrinterSingletonPattern.java
+++ src/main/java/org/techHub/creationalPatterns/PrinterSingletonPattern.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Singleton Pattern. --Ensures that there is only one instance of class is created. provide global point of access to
- * object.
+ * Singleton Pattern. --Ensures that there is only one instance of class is created. provide global point of access to object.
+ * https://www.javacodegeeks.com/2014/05/java-singleton-design-pattern.html?utm_content=buffera4ea7&utm_medium=social&utm_source=facebook.com&utm_campaign=buffer
  *
  * @author darekar
  */
 public class PrinterSingletonPattern {
 
     private static PrinterSingletonPattern singletonPattern;
-    private final List<String> printQueue = new ArrayList<String>();
+    private final List<String> printQueue = new ArrayList<>();
 
     private PrinterSingletonPattern() {
 
